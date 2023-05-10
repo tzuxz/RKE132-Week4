b28105e765fe46b8045152572ede2ec849cd2863
tzuxz <juhanlokkota@gmail.com>
2023-05-10 20:14:48 +0300

    week4

---
+++ ForLoopSum/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+int sum = 0;
+
+for (int i = 0; i < 10; i++) //i-- = i - 1
+{
+
+    Console.WriteLine($"i = {i}");
+    Console.WriteLine($"Current total: {sum}");
+    sum = sum + i; //sum +=i;
+}
+
+//Console.WriteLine($"Final total {sum}");
